98eb48e6b26ccf0b8b522224c24c27cdc9880ed9
Sarun Mukdapitak <sarun.mukda@gmail.com>
2026-04-20 14:54:10 +0900

    add inverted color and luminance function
    Signed-off-by: Sarun Mukdapitak <sarun.mukda@gmail.com>

--- sample1.cpp
+++ sample1.cpp
@@ -1,43 +1,60 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
 #include <stdlib.h>
 #include "myImageIO.h"
 
-int main(int argc, char **argv){
-	
+int main(int argc, char **argv)
+{
+
 	// read image data to img1
-	
-	myImageData * img1 = new myImageData();
+
+	myImageData *img1 = new myImageData();
 	img1->read(argv[1]);
 
 	int W = img1->getWidth();
 	int H = img1->getHeight();
-	
-	printf("resolution: %d x %d\n", W, H);			
-	
+	int CH = img1->getCH();
+
+	printf("resolution: %d x %d\n", W, H);
+	printf("channel: %d\n", CH);
+
 	// prepare img2
-	
-	myImageData *img2  = new myImageData();
-	img2->init(W, H, 1);
+
+	myImageData *img2 = new myImageData();
+	img2->init(W, H, CH);
 
 	// processing
 
-	for(int y = 0; y < H; y++){
-		for(int x = 0; x < W; x++){
-			
-			double value = img1->get(x,y);
-			img2->set(x,y,value);
-			
+	for (int y = 0; y < H; y++)
+	{
+		for (int x = 0; x < W; x++)
+		{
+			for (int channel = 0; channel < CH; channel++)
+			{
+
+				double value = img1->get(x, y, channel);
+
+				// invert luminance of picture
+				double inverted_value = 255 - value;
+				img2->set(x, y, channel, inverted_value);
+			}
 		}
 	}
-		
-	
 
-	img2->save("result");	
-	
-	
+	std::cout << argv[1] << std::endl;
+
+	std::string output_name = "grey_scale_inverted";
+	if (CH == 3)
+	{
+		output_name = "colored_inverted";
+	}
+
+	const char *output_name_char = output_name.c_str();
+	img2->save(output_name_char);
+
 	delete img1;
 	delete img2;
-	
+
 	return 0; // normal termination
-	
 }
